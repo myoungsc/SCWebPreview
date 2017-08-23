abf6f49cb0302ad03d9f9ae6b8a727f30e23c6fe
myoung <myoungsc.dev@gmail.com>
2017-08-24 00:59:21 +0900

    dummy file update

--- Example/SCWebPreview/dummyfile1/dummyfile1.swift
+++ Example/SCWebPreview/dummyfile1/dummyfile1.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class dummyfile1: NSObject {
     
-    /*
+    /*      
      MIT LICENSE
      
      Copyright (c) 2017 myoungsc <myoungsc.dev@gmail.com>
@@ -30,77 +30,8 @@ class dummyfile1: NSObject {
      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+     OUT OF OR IN CON
      THE SOFTWARE.
      
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
      */
-
-
-
-
-
-
-
-
 }
