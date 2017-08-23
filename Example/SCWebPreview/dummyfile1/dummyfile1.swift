686f057b2d04de1275e6cb9e65f6e5d84c9ae395
myoung <myoungsc.dev@gmail.com>
2017-08-24 00:48:26 +0900

    Add dummy file

---
+++ Example/SCWebPreview/dummyfile1/dummyfile1.swift
@@ -0,0 +1,13 @@
+//
+//  dummyfile1.swift
+//  SCWebPreview
+//
+//  Created by myoung on 2017. 8. 24..
+//  Copyright © 2017년 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class dummyfile1: NSObject {
+
+}
---
+++ Example/SCWebPreview/dummyfile1/dummyfile2.swift
@@ -0,0 +1,19 @@
+//
+//  dummyfile2.swift
+//  SCWebPreview
+//
+//  Created by myoung on 2017. 8. 24..
+//  Copyright © 2017년 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class dummyfile2: NSObject {
+    
+    /*
+     Hi. my name is myoungsc
+        iOS Developer (2~3)year
+            but very very difficult....
+     */
+    
+}
